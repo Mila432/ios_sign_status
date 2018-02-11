fdc774467d4fbc999db6f2a8e3144ed0ea58fa1c
admin <root@local>
2018-02-11 17:46:37 +0100

    ota

--- ios.py
+++ ios.py
@@ -3,25 +3,43 @@ import plistlib
 
 ioses={}
 
-r=requests.get('https://s.mzstatic.com/version')
-p=plistlib.readPlistFromString(r.content)
-for i in p:
-	if i =='MobileDeviceSoftwareVersionsByVersion':
-		versions=p[i]
-		for v in versions:
-			for z in versions[v]:
-				if z =='MobileDeviceSoftwareVersions':
-					for t in versions[v][z]:
-						for mv in versions[v][z][t]:
-							if 'SameAs' in versions[v][z][t][mv] or 'Universal' in versions[v][z][t][mv] or 'Restore' not in versions[v][z][t][mv]:
-								continue
-							if t in ioses:
-								if versions[v][z][t][mv]['Restore']['ProductVersion'] not in ioses[t]:
+def ios_ipsw():
+	r=requests.get('https://s.mzstatic.com/version')
+	p=plistlib.readPlistFromString(r.content)
+	for i in p:
+		if i =='MobileDeviceSoftwareVersionsByVersion':
+			versions=p[i]
+			for v in versions:
+				for z in versions[v]:
+					if z =='MobileDeviceSoftwareVersions':
+						for t in versions[v][z]:
+							for mv in versions[v][z][t]:
+								if 'SameAs' in versions[v][z][t][mv] or 'Universal' in versions[v][z][t][mv] or 'Restore' not in versions[v][z][t][mv]:
+									continue
+								if t in ioses:
+									if versions[v][z][t][mv]['Restore']['ProductVersion'] not in ioses[t]:
+										ioses[t].append(versions[v][z][t][mv]['Restore']['ProductVersion'])
+								else:
+									ioses[t]=[]
 									ioses[t].append(versions[v][z][t][mv]['Restore']['ProductVersion'])
-							else:
-								ioses[t]=[]
-								ioses[t].append(versions[v][z][t][mv]['Restore']['ProductVersion'])
-							#print versions[v][z][t][mv]['Restore']['ProductVersion'],t
+								#print versions[v][z][t][mv]['Restore']['ProductVersion'],t
+
+def ios_ota():
+	r=requests.get('https://mesu.apple.com/assets/com_apple_MobileAsset_SoftwareUpdate/com_apple_MobileAsset_SoftwareUpdate.xml')
+	p=plistlib.readPlistFromString(r.content)
+	for i in p:
+		if i =='Assets':
+			versions=p[i]
+			for v in versions:
+				ver=v['OSVersion'].replace('9.9.','')
+				for t in v['SupportedDevices']:
+					if t in ioses:
+						if ver not in ioses[t]:
+							ioses[t].append(ver)
+					else:
+						ioses[t]=[]
+						ioses[t].append(ver)
+
 def ml(l):
 	tmp=[]
 	tt=[]
@@ -38,6 +56,10 @@ def ml(l):
 	for x in sorted(tmp,key=lambda x: x[0]):
 		tt.append(x[1])
 	return tt
+
+ios_ipsw()
+ios_ota()
+
 tmp=[]							
 for de in ioses:
 	_t=ml([x for x in ioses[de]])
